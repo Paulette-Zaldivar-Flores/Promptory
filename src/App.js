6a309b5c234514a285b8eab900e618d6c569cc55
Void321 <96970580+Void321@users.noreply.github.com>
2022-05-03 21:12:47 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import world from "./world.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={world} className="map img-fluid" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
