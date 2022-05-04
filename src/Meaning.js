7b6bccdbb8ce707713af15ba8c68271390b68c74
Void321 <96970580+Void321@users.noreply.github.com>
2022-05-04 21:05:24 +0900

    Update Meaning.js
    -developed loop definition, index
    -display definitions
    -display example of definition

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,6 +5,17 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
