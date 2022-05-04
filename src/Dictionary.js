b597b2fcc068debe9da44524a8feabea059dc2d1
Void321 <96970580+Void321@users.noreply.github.com>
2022-05-04 15:12:02 +0900

    Dictionary.js
    -added axios
    -integrated dictionary api
    -function handle response
    -https://dictionaryapi.dev/ (API documentation url)

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -6,7 +7,13 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} `);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
 
   function handleKeywordChange(event) {
