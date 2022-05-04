981bdb867a59ceac6b1b15ba5fd471dffbecdbd8
Void321 <96970580+Void321@users.noreply.github.com>
2022-05-04 20:45:24 +0900

    Dictionary.js, Meaning.js, Results.js
    Dictionary.js- created new state for results and imported results component
    Results.js- created component, meaning, looped through meanings, imported meaning component
    Meaning.j-created componenent which includes parts of speech

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function search(event) {
     event.preventDefault();
@@ -14,6 +16,7 @@ export default function Dictionary() {
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function handleKeywordChange(event) {
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
