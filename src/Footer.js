12c924645d7f2a972d4d56e4302a245cb1f06649
Void321 <paulettz@icloud.com>
2022-06-08 22:35:13 +0900

    Updating contact links on footer
    -imported font awesome in footer.js
    -added linked in and github icons on the footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,31 @@
 import React from "react";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <div className="Footer">
       <footer className="footer">
+        Open-source code
         <a
           href="https://github.com/Void321/React-Dictionary"
           target="_blank"
           rel="noreferrer"
         >
-          Open-source code
+          {" "}
+          <FontAwesomeIcon icon={faGithubAlt}></FontAwesomeIcon>
         </a>{" "}
-        |by: Paulette Zaldivar Flores | Hosted on{" "}
+        |by: Me{" "}
+        <a
+          href="https://www.linkedin.com/in/paulettezaldivarflores3"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+        </a>{" "}
+        | Hosted on{" "}
         <a
           href="https://incandescent-froyo-b0949f.netlify.app/"
           target="blank"
